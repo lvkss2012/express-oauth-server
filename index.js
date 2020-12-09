c90ffad3dbbcb7aa62a6208ea0ba111b7fb24d02
刁世杰 <805386312@qq.com>
2020-12-09 14:23:31 +0800

    失败返回值中添加code，升级依赖库

--- index.js
+++ index.js
@@ -16,19 +16,19 @@ var UnauthorizedRequestError = require('oauth2-server/lib/errors/unauthorized-re
  */
 
 function ExpressOAuthServer(options) {
-  options = options || {};
+    options = options || {};
 
-  if (!options.model) {
-    throw new InvalidArgumentError('Missing parameter: `model`');
-  }
+    if (!options.model) {
+        throw new InvalidArgumentError('Missing parameter: `model`');
+    }
 
-  this.useErrorHandler = options.useErrorHandler ? true : false;
-  delete options.useErrorHandler;
+    this.useErrorHandler = options.useErrorHandler ? true : false;
+    delete options.useErrorHandler;
 
-  this.continueMiddleware = options.continueMiddleware ? true : false;
-  delete options.continueMiddleware;
+    this.continueMiddleware = options.continueMiddleware ? true : false;
+    delete options.continueMiddleware;
 
-  this.server = new NodeOAuthServer(options);
+    this.server = new NodeOAuthServer(options);
 }
 
 /**
@@ -39,24 +39,24 @@ function ExpressOAuthServer(options) {
  * (See: https://tools.ietf.org/html/rfc6749#section-7)
  */
 
-ExpressOAuthServer.prototype.authenticate = function(options) {
-  var that = this;
+ExpressOAuthServer.prototype.authenticate = function (options) {
+    var that = this;
 
-  return function(req, res, next) {
-    var request = new Request(req);
-    var response = new Response(res);
-    return Promise.bind(that)
-      .then(function() {
-        return this.server.authenticate(request, response, options);
-      })
-      .tap(function(token) {
-        res.locals.oauth = { token: token };
-        next();
-      })
-      .catch(function(e) {
-        return handleError.call(this, e, req, res, null, next);
-      });
-  };
+    return function (req, res, next) {
+        var request = new Request(req);
+        var response = new Response(res);
+        return Promise.bind(that)
+            .then(function () {
+                return this.server.authenticate(request, response, options);
+            })
+            .tap(function (token) {
+                res.locals.oauth = {token: token};
+                next();
+            })
+            .catch(function (e) {
+                return handleError.call(this, e, req, res, null, next);
+            });
+    };
 };
 
 /**
@@ -67,30 +67,30 @@ ExpressOAuthServer.prototype.authenticate = function(options) {
  * (See: https://tools.ietf.org/html/rfc6749#section-3.1)
  */
 
-ExpressOAuthServer.prototype.authorize = function(options) {
-  var that = this;
+ExpressOAuthServer.prototype.authorize = function (options) {
+    var that = this;
 
-  return function(req, res, next) {
-    var request = new Request(req);
-    var response = new Response(res);
+    return function (req, res, next) {
+        var request = new Request(req);
+        var response = new Response(res);
 
-    return Promise.bind(that)
-      .then(function() {
-        return this.server.authorize(request, response, options);
-      })
-      .tap(function(code) {
-        res.locals.oauth = { code: code };
-        if (this.continueMiddleware) {
-          next();
-        }
-      })
-      .then(function() {
-        return handleResponse.call(this, req, res, response);
-      })
-      .catch(function(e) {
-        return handleError.call(this, e, req, res, response, next);
-      });
-  };
+        return Promise.bind(that)
+            .then(function () {
+                return this.server.authorize(request, response, options);
+            })
+            .tap(function (code) {
+                res.locals.oauth = {code: code};
+                if (this.continueMiddleware) {
+                    next();
+                }
+            })
+            .then(function () {
+                return handleResponse.call(this, req, res, response);
+            })
+            .catch(function (e) {
+                return handleError.call(this, e, req, res, response, next);
+            });
+    };
 };
 
 /**
@@ -101,69 +101,69 @@ ExpressOAuthServer.prototype.authorize = function(options) {
  * (See: https://tools.ietf.org/html/rfc6749#section-3.2)
  */
 
-ExpressOAuthServer.prototype.token = function(options) {
-  var that = this;
+ExpressOAuthServer.prototype.token = function (options) {
+    var that = this;
 
-  return function(req, res, next) {
-    var request = new Request(req);
-    var response = new Response(res);
+    return function (req, res, next) {
+        var request = new Request(req);
+        var response = new Response(res);
 
-    return Promise.bind(that)
-      .then(function() {
-        return this.server.token(request, response, options);
-      })
-      .tap(function(token) {
-        res.locals.oauth = { token: token };
-        if (this.continueMiddleware) {
-          next();
-        }
-      })
-      .then(function() {
-        return handleResponse.call(this, req, res, response);
-      })
-      .catch(function(e) {
-        return handleError.call(this, e, req, res, response, next);
-      });
-  };
+        return Promise.bind(that)
+            .then(function () {
+                return this.server.token(request, response, options);
+            })
+            .tap(function (token) {
+                res.locals.oauth = {token: token};
+                if (this.continueMiddleware) {
+                    next();
+                }
+            })
+            .then(function () {
+                return handleResponse.call(this, req, res, response);
+            })
+            .catch(function (e) {
+                return handleError.call(this, e, req, res, response, next);
+            });
+    };
 };
 
 /**
  * Handle response.
  */
-var handleResponse = function(req, res, response) {
+var handleResponse = function (req, res, response) {
 
-  if (response.status === 302) {
-    var location = response.headers.location;
-    delete response.headers.location;
-    res.set(response.headers);
-    res.redirect(location);
-  } else {
-    res.set(response.headers);
-    res.status(response.status).send(response.body);
-  }
+    if (response.status === 302) {
+        var location = response.headers.location;
+        delete response.headers.location;
+        res.set(response.headers);
+        res.redirect(location);
+    } else {
+        res.set(response.headers);
+        res.status(response.status).send(response.body);
+    }
 };
 
 /**
  * Handle error.
  */
 
-var handleError = function(e, req, res, response, next) {
+var handleError = function (e, req, res, response, next) {
 
-  if (this.useErrorHandler === true) {
-    next(e);
-  } else {
-    if (response) {
-      res.set(response.headers);
+    if (this.useErrorHandler === true) {
+        next(e);
+    } else {
+        if (response) {
+            res.set(response.headers);
+        }
+
+        res.status(e.code);
+
+        if (e instanceof UnauthorizedRequestError) {
+            return res.send();
+        }
+
+        res.send({error: e.name, error_description: e.message, code: 1});
     }
-
-    res.status(e.code);
-
-    if (e instanceof UnauthorizedRequestError) {
-      return res.send();
-    }
-
-    res.send({ error: e.name, error_description: e.message });
-  }
 };
 
 /**
